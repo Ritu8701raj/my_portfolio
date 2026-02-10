edd76ded2fa75d835d5952623a57408685141383
Rituraj kumar <ritu61523@gmail.com>
2026-02-10 22:23:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({
-        behavior: "smooth"
+function showSection(sectionId){
+
+    const sections = document.querySelectorAll(".section");
+
+    sections.forEach(section => {
+        section.classList.remove("active");
     });
+
+    document.getElementById(sectionId).classList.add("active");
 }
